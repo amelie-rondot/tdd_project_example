5e888ac7fef39518d53016e19630c9edc7a10f78
Amélie Rondot <amelie.rondot@multi.coop>
2024-10-21 12:34:59 +0200

    🟢 TDD: small implementation for age = 18

--- tdd-example/tdd_example/main.py
+++ tdd-example/tdd_example/main.py
@@ -1,3 +1,5 @@
 def start():
     print("Bonjour et bienvenue dans le programme Tarif' !")
-    print("Il permet de déterminer le tarif du billet de spectacle en fonction de l'âge du spectateur ou de la spectatrice.")
+    print(
+        "Il permet de déterminer le tarif du billet de spectacle en fonction de l'âge du spectateur ou de la spectatrice."
+    )
--- tdd-example/tdd_example/tarification.py
+++ tdd-example/tdd_example/tarification.py
@@ -1,2 +1,6 @@
 def get_price(age: int) -> int:
-    return 0
+    if age == 0:
+        return 0
+    elif age ==18:
+        raise Exception("L'age de la personne que vous avez rensigné, "
+                        f"{age} ans, n'est pas compris entre 0 et 17 ans inclus.")
