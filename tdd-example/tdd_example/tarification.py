75bf8964589e310b1bbd514bcab958a9279bc59f
Amélie Rondot <amelie.rondot@multi.coop>
2024-10-21 12:58:54 +0200

    🟢 TDD: small implements for age = 17

--- tdd-example/tdd_example/tarification.py
+++ tdd-example/tdd_example/tarification.py
@@ -1,6 +1,8 @@
 def get_price(age: int) -> int:
     if age == 0:
         return 0
+    elif age == 17:
+        return 0
     elif age ==18:
         raise Exception(
             "L'age de la personne que vous avez renseigné, "
